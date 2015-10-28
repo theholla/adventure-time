fea6fc99fdf46f8bec56931ef60fa412c5be6f9b
diana holland & summer brochtrup <student@epicodus.com>
2015-10-28 10:10:44 -0700

    Fix imports

--- app/src/main/java/com/summer/and/diana/adventuretime/LoginActivity.java
+++ app/src/main/java/com/summer/and/diana/adventuretime/LoginActivity.java
@@ -2,8 +2,6 @@ package com.summer.and.diana.adventuretime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
